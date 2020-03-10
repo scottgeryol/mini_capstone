9566fcded334b808db94ff49161b6ef556d8c791
Scott Geryol <sgeryol@gmail.com>
2020-03-09 19:45:28 -0500

    Changed Description Attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_003520) do
+ActiveRecord::Schema.define(version: 2020_03_10_004247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_003520) do
     t.string "name"
     t.integer "price"
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.string "color"
     t.integer "weight"
     t.string "tracklist"
